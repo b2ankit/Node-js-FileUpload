834c37df118e37e0a75796193e239986dee3536f
B2Ankit <ankit15ec007@gmail.com>
2019-12-28 22:06:17 +0530

    unifiedTopolgy added

--- modules/upload.js
+++ modules/upload.js
@@ -2,7 +2,7 @@
 var mongoose = require('mongoose');         
 
 //connect mongoose data base using connect('url',{useNewUrlParser}) method
-mongoose.connect('mongodb://localhost:27017/employee', {useNewUrlParser: true});  
+mongoose.connect('mongodb://localhost:27017/employee', {useNewUrlParser: true,useUnifiedTopology: true});  
 
 //creating a object of connection
 var conn = mongoose.connection;
